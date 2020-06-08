bf45e854d1fa6ce9bc923a26243636032a120556
nboyse <natasha.boyse@digital.trade.gov.uk>
2020-06-08 16:10:39 +0100

    Test format

--- authbroker_client/middleware.py
+++ authbroker_client/middleware.py
@@ -14,9 +14,9 @@ class ProtectAllViewsMiddleware:
 
     def __call__(self, request):
         if (
-            request.path not in self.anonymous_paths and 
-            resolve(request.path).app_name != 'authbroker_client' and not 
-            request.user.is_authenticated
+                request.path not in self.anonymous_paths and
+                resolve(request.path).app_name != 'authbroker_client' and not
+                request.user.is_authenticated
         ):
             return redirect('authbroker_client:login')
 
