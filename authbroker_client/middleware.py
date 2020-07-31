7a1d9c78b602f5a5f2b67a38e9b58eb47c03ff52
nboyse <natasha.boyse@digital.trade.gov.uk>
2020-07-31 15:22:31 +0100

    Debug path issue

--- authbroker_client/middleware.py
+++ authbroker_client/middleware.py
@@ -15,9 +15,8 @@ class ProtectAllViewsMiddleware:
     def __call__(self, request):
         print("hello")
         print("request.path", request.path)
-        assert False
         if (
-            str(resolve(request.path)) not in self.anonymous_paths and
+            request.path not in self.anonymous_paths and
             resolve(request.path).app_name != 'authbroker_client' and not
             request.user.is_authenticated
         ):
