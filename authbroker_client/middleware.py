174380c5643b2620dec5c7dee4f600a3b95ab81c
nboyse <natasha.boyse@digital.trade.gov.uk>
2020-07-31 15:56:44 +0100

    flake8

--- authbroker_client/middleware.py
+++ authbroker_client/middleware.py
@@ -13,8 +13,9 @@ class ProtectAllViewsMiddleware:
         )
 
     def __call__(self, request):
-        if (  #noqa W504
-            request.path not in self.anonymous_paths and
+        if (
+            request.path not in
+            self.anonymous_paths and
             resolve(request.path).app_name != 'authbroker_client' and not
             request.user.is_authenticated
         ):
