bb685efa9d5e91858b3c408ac6972e246f0d5a95
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2024-06-12 15:49:51 +0100

    Add some comments

--- authbroker_client/version.py
+++ authbroker_client/version.py
@@ -1,1 +1,2 @@
+# The version of the package
 __version__ = "4.3.0"
--- tests/test_version.py
+++ tests/test_version.py
@@ -2,4 +2,5 @@ from authbroker_client.version import __version__
 
 
 def test_version():
+    # Tests the version of the package
     assert __version__ == "4.3.0"
